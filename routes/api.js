f2073e0a0e05e44b7043e391b727badd8edf7b4b
Deepkumar Patel <dspguru97@gmail.com>
2021-03-10 15:35:11 -0500

    gitIgnore

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+//import sql conncetion
+const connect = require("../config/sqlConfig");
 
 router.get("/", (req, res)=>{
     //res.json = echo json_encode in PHP
@@ -15,10 +17,43 @@ router.get("/users", (req, res)=>{
 //Dyncamic route handler that can accept a parameter
 //this equal to $_GET["id"] (req.params.id)
 //we are passing the id via route : /api/movies/20 etc
+router.get("/movies", (req, res)=>{
+    // run sql query here
+
+    connect.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+        
+        // Use the connection
+        connection.query('SELECT * FROM tbl_movies', function (error, results) {
+            // When done with the connection, release it.
+            connection.release();
+        
+            // Handle error after the release.
+            if (error) throw error;
+
+            res.json(results);
+        });
+    });
+    //res.json( query result here )
+});
+
+
 router.get("/movies/:id", (req, res)=>{
     // run sql query here
-    //res.json( query result here )
-    res.json({message: "all movies route", movie:req.params.id});
+    connect.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+        
+        // Use the connection
+        connection.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function (error, results) {
+            // When done with the connection, release it.
+            connection.release();
+        
+            // Handle error after the release.
+            if (error) throw error;
+    
+            res.json(results);
+        });
+        });
 });
 
 
